003d6dbc1c1d200e597277f918b99fa49f50a414
Samreen Azam <sa3tnc@virginia.edu>
2022-05-07 11:59:10 -0400

    display most to least popular lines on view all page

--- src/app/linesOfBusiness/linesOfBusiness.component.ts
+++ src/app/linesOfBusiness/linesOfBusiness.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs/internal/observable/forkJoin';
 
 import { LineOfBusiness } from '../LineOfBusiness';
 import { LineOfBusinessService } from '../lineOfBusiness.service';
@@ -18,9 +19,27 @@ export class LineOfBusinessComponent implements OnInit {
   }
 
   getLinesOfBusiness(): void {
-    this.lineOfBusinessService.getLinesOfBusiness()
-    .subscribe(linesOfBusiness => this.linesOfBusiness = linesOfBusiness);
+    // for coding challenge:
+    // using fork join to associate each line with its quotes
+     
+    forkJoin([
+        this.lineOfBusinessService.getLinesOfBusiness(),
+        this.lineOfBusinessService.getRecentQuotes(),
+      ]).subscribe(([linesOfBusiness, quotesList]) => {
+        this.linesOfBusiness = linesOfBusiness;
+
+        linesOfBusiness.forEach( line => {
+          let quoteCount = 0;
+          quotesList.forEach(q =>  { 
+            if (q.lineOfBusiness === line.id) { 
+              quoteCount += 1; 
+            }
+          })
+          line.numQuotes = quoteCount; 
+        });
+      });
   }
+
 
   add(name: string, description: string): void {
     name = name.trim();
@@ -34,6 +53,17 @@ export class LineOfBusinessComponent implements OnInit {
   delete(lineOfBusiness: LineOfBusiness): void {
     this.linesOfBusiness = this.linesOfBusiness.filter(lob => lob !== lineOfBusiness);
     this.lineOfBusinessService.deleteLineOfBusiness(lineOfBusiness.id).subscribe();
+  }
+
+  // for displaying by popularity:
+  sortByPopularity(): void {
+    this.linesOfBusiness.sort((l1, l2) => l1.numQuotes > l2.numQuotes ? 
+      -1 : l1.numQuotes < l2.numQuotes ? 1 : 0);
+  }
+
+  // for displaying by id:
+  sortByID(): void {
+    this.linesOfBusiness.sort((l1, l2) => l1.id < l2.id ? -1 : l1.id > l2.id ? 1 : 0);
   }
 
 }
